844d7f689ad7079eab0bc0b8b63632f537f15df3
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-04-22 19:26:43 +0530

    Time: 8 ms (26.39%), Space: 42.3 MB (47.87%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,22 +1,6 @@
 class Solution {
     
-    
-    
-    
-//     public boolean isPalindrome(String x)
-//     {  for(int i=0;i<=x.length()/2;i++)
-//         {
-//             if(x.charAt(i)!=x.charAt(x.length()-i-1))
-//             {
-                
-//                 return false;
-                
-//             }
-   
-//         }
-//      System.out.println("palindrome is "+x);
-//      return true;
-//     }
+
    
     public int longestPalindrome(String s) {
         int cnt=0;
@@ -25,7 +9,7 @@ class Solution {
         Arrays.sort(arr);
         
         Stack<Character> st=new Stack<Character>();
-       //System.out.println("Arrays after sort"+ arr.toString());
+       
         for(Character x:arr)
         {
             if(st.size()==0)
