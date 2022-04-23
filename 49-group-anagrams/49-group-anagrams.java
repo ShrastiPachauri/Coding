bd03a9ca18542e906c7b8d0153aafab242be1bcd
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-04-23 11:34:03 +0530

    Time: 1960 ms (5.02%), Space: 46 MB (81.24%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,71 @@
+class Solution {
+    
+    
+    
+    
+    public boolean isAnagram(String input1,String input2)
+    {
+        if(input1.length()!=input2.length())
+        {
+            return false;
+        }
+        
+        char[] arr1=input1.toCharArray();
+         char[] arr2=input2.toCharArray();
+        
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    if(Arrays.equals(arr1,arr2))
+        {
+          return true;  
+        }
+        
+        return false;
+    }
+    
+  
+    
+    
+    public List<List<String>> groupAnagrams(String[] strs) {
+       HashSet<Integer> visited=new HashSet<Integer>();
+        
+        List<List<String>> ans=new ArrayList<List<String>>();
+        
+        for(int i=0;i<strs.length;i++)
+        {
+            if(visited.contains(i))
+            {
+                continue;
+            }
+            int flag=0;
+             List<String> sub=new ArrayList<String>();
+           for(int j=i+1;j<strs.length;j++)
+        {
+           
+             
+               if(!visited.contains(j))
+               {
+            if(isAnagram(strs[i],strs[j]))
+            { flag=1;
+             //stem.out.println("added "+strs[j]+"in"+strs[i]);
+             sub.add(strs[j]);
+             visited.add(j);
+            }
+            
+               }
+            
+           } 
+            
+                 sub.add(strs[i]);
+                 visited.add(i);
+           
+           
+            ans.add(sub);
+        }
+        
+        return ans;
+        
+        
+        
+    }
+}
