202c507f89ffa727a5749b3a4b83772c53a8d3cc
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-07-21 10:49:52 +0530

    Time: 2 ms (85.31%), Space: 55.1 MB (75.90%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,12 +1,33 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[]ans=new int[nums.length];
-        int j=0;
-        for(int i=0;i<nums.length;i++)
+        //since array is already sorted the max element after sorting would be at eithr of end so taking up two pointer approach
+        
+        
+      int i=0;
+        int j=nums.length-1;
+        int []ans=new int[nums.length];
+        int k=nums.length-1;
+        while(i<=j && k>=0)
         {
-            ans[j++]=(int)Math.pow(nums[i],2);
+        if(Math.abs(nums[i])<Math.abs(nums[j]))
+        {
+            ans[k]=nums[j]*nums[j];
+            j--;
+            k--;
         }
-        Arrays.sort(ans);
+        else
+        {
+            ans[k]=nums[i]*nums[i];
+            i++;
+            k--;
+        }
+        
+        }
+        
         return ans;
+        
+        
+        
+        
     }
 }
