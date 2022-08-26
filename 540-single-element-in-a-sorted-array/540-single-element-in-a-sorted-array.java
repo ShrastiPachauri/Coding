699cbe31c99749436d569fe0ba65ce8fca407228
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-08-26 15:37:10 +0530

    Time: 5 ms (6.70%), Space: 60.7 MB (30.90%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int singleNonDuplicate(int[] arr) {
+        
+        Arrays.sort(arr);//sorted
+        //System.out.println("sorted array "+arr);
+      if(arr.length==1)
+      {
+          return arr[0];
+      }
+        
+        for(int i=0;i<arr.length-1;)
+        {
+          
+            if(arr[i]!=arr[i+1] )
+            {
+                return arr[i];//unique
+            }
+            
+                i+=2;
+            
+        }
+        //means nothing returned so far,odd list no unique found till now
+        
+        return arr[arr.length-1]; //last element of odd list
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
