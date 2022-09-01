eac680542a50574c6696e4d266045fddd930ca11
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-09-01 17:40:48 +0530

    Time: 2 ms (61.17%), Space: 42.3 MB (27.39%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -1,54 +1,55 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] arr1=version1.split("\\.");
-        String[]arr2=version2.split("\\.");
         
+       String[]v1=version1.split("\\.");
         
-       // System.out.println("lengths of arr1 and 2 are "+arr1.length+" "+arr2.length);
-        
-        
-        
-        
+        String[]v2=version2.split("\\.");
+      
+        int x=0,y=0;
         int i=0;
-        
-        
-        for(i=0;i<arr1.length;i++)
+       
+        for(;i<v1.length;i++)
         {
+            if(i==v2.length)break;
+             x=Integer.parseInt(v1[i]);
+           
+            y=Integer.parseInt(v2[i]);
             
-                if(i>=arr2.length)
-                   {
-                    if(Integer.parseInt(arr1[i])>0)
-                    {return 1;}
-                    else
-                    {
-                        continue;
-                    }
-                   
-                }
-//System.out.println("after parsing"+Integer.parseInt(arr1[i])+" "+Integer.parseInt(arr2[i]));
-            if(Integer.parseInt(arr1[i])<Integer.parseInt(arr2[i]))
-            {
-                return -1;
-            }
-            else if(Integer.parseInt(arr1[i])>Integer.parseInt(arr2[i]))
+            if(x>y)
             {
                 return 1;
             }
-            else
-                continue;
-        }
-        
-        if(arr2.length>arr1.length)
-        {
-            while(i<arr2.length)
-            {if(Integer.parseInt(arr2[i])>0)
-                   {
-                       return -1;
-                   }
-             i++;
+            else if(x<y)
+            {
+                return -1;
             }
-            
         }
+        //System.out.println("i is "+i+" v2 length is "+v2.length);
+        
+         while(i<v1.length)
+       {
+           if(Integer.parseInt(v1[i])>0)
+           {
+               return 1;
+           }
+           i++;
+       }
+        
+        
+        
+        
+        
+        
+       while(i<v2.length)
+       {
+           if(Integer.parseInt(v2[i])>0)
+           {
+               return -1;
+           }
+           i++;
+       }
+        
+        
         
         return 0;
         
