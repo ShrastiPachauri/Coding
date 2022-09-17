2cfbe1541a847d8504080a34b3a0a67b72afd35d
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-09-17 09:36:57 +0530

    Time: 20 ms (5.07%), Space: 43.5 MB (21.65%) - LeetHub

--- 61-rotate-list/61-rotate-list.java
+++ 61-rotate-list/61-rotate-list.java
@@ -10,56 +10,40 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        
-        if(head==null)
-        {
-            return null;
-        }
-        
-        int cnt=0;
-        
-        ListNode temp=head;
-        
-        while(temp!=null)
-        {
-            cnt++;
-            temp=temp.next;
-            
-        }
-        
-        temp=head;//reset temp
-        ListNode t=head;//head node
-        int ti=0;
-        int K=k%cnt;//net effective rotation
-        if(K==0) //no rotation
-        {
-            return head;
-        }
-        
-        
-        
-        
-         while(true)
-        {
-             ti++;
-             if(ti==cnt-K)
-             {
-                 break;
-             }
-            temp=temp.next;
-            
-        }
-        
-        ListNode n=temp.next;//starting of breakpoint
-        temp.next=null;//end of list
-        head=n;//reset head
-        while(n.next!=null)
-        {
-            n=n.next;
-        }
-        n.next=t;
-        
-        return head;
-        
+        if(head==null||k==0)return head;
+       int count=0;
+
+ListNode temp=head;
+
+while(temp!=null)//to count nodes
+{
+temp=temp.next;
+count++;
+}
+//count is total nodes in list
+if(k>=count)k=k%count;//efective rotation
+        System.out.println("count is "+count +"k is "+k);
+if(k==0)return head;
+count=count-k;
+temp=head;
+
+while(count-->1)
+{
+temp=temp.next;
+
+}
+ListNode newhead=temp.next;
+temp.next=null;
+temp=newhead;
+
+while(temp.next!=null)
+{
+temp=temp.next;
+
+}
+temp.next=head;
+head=newhead;
+
+return head;
     }
 }
