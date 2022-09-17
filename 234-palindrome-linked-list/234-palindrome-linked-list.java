8b8301f775d9cc5757910cb5cf02caab165801fd
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-09-17 23:47:55 +0530

    Time: 8 ms (59.06%), Space: 97.4 MB (63.15%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -11,60 +11,51 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         
-        if(head==null)
-        {
-            return false;
-        }
-        ListNode slow=head;
-        ListNode fast=head;
-        Stack<Integer> s=new Stack<Integer>();
+if(head==null)return true;
+int count=0;
+
+ListNode temp=head;
+while(temp!=null)
+{
+temp=temp.next;
+count++;
+}
+if(count<=1)return true; 
+if(count==2 )
+{if(head.val==head.next.val)return true;
+return false;
+}
         
-      int cnt=1;
-        while(fast!=null )
-        {s.push(slow.val);
-       
-        
-            slow=slow.next;
-           
-           fast=fast.next;
-         
-            if(fast!=null)
-            {
-                cnt++;
-                fast=fast.next;
-                if(fast!=null)
-                {
-                    cnt++;
-                }
-                
-            }
-            
-        }
-        // System.out.println("cnt "+cnt);
-        if(cnt<=1)
-        {
-            return true;
-        }
-       if(cnt%2!=0)
-       {
-           s.pop();//pop the centre unique value
-       }
-        while(slow!=null)
-            {
-            
-            if(s.peek()!=slow.val)
-            {
-                return false;
-            }
-            s.pop();
-                slow=slow.next;
-            }
-        
-        if(s.size()!=0)
-        {
-            return false;
-        }
-        return true;
-        
+count=count/2;  //n=4,cnt=2
+
+temp=head;
+while(count-->0)/**good point -->**/
+{
+temp=temp.next;
+}
+//reverse
+
+ListNode prev=temp;
+ListNode curr=temp.next;
+ListNode next=curr.next;
+
+while(curr!=null)
+{
+next=curr.next;
+curr.next=prev;
+prev=curr;
+curr=next;
+
+}
+//prev node is tail node to  traverse in reverse
+temp=head;//reset
+while(temp.val==prev.val)
+{
+temp=temp.next;
+prev=prev.next;
+if(temp==prev||prev.next==temp)return true;
+}
+
+return false;
     }
 }
