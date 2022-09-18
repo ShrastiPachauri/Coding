5502d50b7806a10b021475d617dec3ac351755ff
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-09-18 15:08:20 +0530

    Time: 1 ms (66.48%), Space: 42.3 MB (47.09%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -18,14 +18,12 @@ class Solution {
     
     public boolean checkSymmetry(TreeNode root1,TreeNode root2)
     {
-        if(root1==null&&root2==null)return true;
-        if((root1==null&&root2!=null)||root1!=null&&root2==null)return false;
-        
-       if(root1.val!=root2.val)return false;
-        
-        
-        return checkSymmetry(root1.left,root2.right)&&checkSymmetry(root1.right,root2.left);
-        
+        if(root1==null && root2==null)return true;
+if(root1==null || root2==null)return false;
+if(root1.val!=root2.val)return false;
+
+return(checkSymmetry(root1.left,root2.right)&&checkSymmetry(root1.right,root2.left));
+
     }
     
     
