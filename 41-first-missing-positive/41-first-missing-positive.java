f6a40a25cf3731017887bca31a3abd1328746554
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-09-21 10:27:58 +0530

    Time: 2 ms (90.16%), Space: 57.8 MB (68.10%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,32 +1,48 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        HashSet<Integer> hs=new HashSet<Integer>();
-       
-         for(int i=0;i<nums.length;i++)
-        {
-            hs.add(nums[i]);
-            
-            
-        }
-       
-        for(int i=1;i<=nums.length;i++)
-        {
-            
-            if(!hs.contains(i))
-            {
-               return i;
-            }
-            
-        }
-        
-        return nums.length+1;
-        
-    
-        
-        
-        
-        
-        
-        
-    }
+for(int i=0;i<nums.length;i++)
+{
+if(nums[i]<=0||nums[i]>nums.length)
+{
+nums[i]=nums.length+2;//so that can identify non req values
 }
+}
+
+for(int i=0;i<nums.length;i++)
+{
+if( nums[i]==nums.length+2)//don’t mark for neg or very large value
+{
+continue;
+}
+
+if(nums[i]<0)
+{
+int  x=-1*nums[i];
+    if(x==nums.length+2)continue;
+
+    if(nums[x-1]>0)//not marked yet ,for repeated values
+    {nums[x-1]=-1*nums[x-1];}
+}
+else
+{
+    if(nums[nums[i]-1]>0)//not marked yet ,for repeated values
+    {nums[nums[i]-1]=-1*nums[nums[i]-1];}
+}
+
+}
+
+
+
+
+for(int i=0;i<nums.length;i++)
+{
+if(nums[i]>0)
+{
+return i+1;
+}
+}
+        return nums.length+1;
+}
+
+    
+    }
