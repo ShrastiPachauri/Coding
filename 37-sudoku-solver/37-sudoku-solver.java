26e66131dc1d4a93cb18a8279fae84f9b2334577
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-09-21 11:30:24 +0530

    Time: 24 ms (33.19%), Space: 41.4 MB (65.54%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -1,63 +1,49 @@
 class Solution {
-    
-    
-    public boolean solve(char[][]board)
-    {
-        
-        for(int i=0;i<board.length;i++)
-        {
-            for(int j=0;j<board[0].length;j++)
-            {
-                if(board[i][j]=='.')//first empty cell ,others will take care by recursion
-                {
-                    for(char ch='1';ch<='9';ch++)
-                    {
-                        if(isValid(board,ch,i,j))
-                        {
-                            
-                            board[i][j]=ch;
-                            
-                            if(solve(board))return true;
-                            
-                            board[i][j]='.';//undo b/c this choice is not giving ans
-                            
-                        }
-                    }
-                    return false;
-                    
-                    
-                    
-                }
-            }
-            
-        }
-        return true;
-        
-    }
-    
-    
-    public boolean isValid(char[][]board,int ch,int row,int col)
-    {
-        
-        for(int i=0;i<9;i++)
-        {
-            
-            if(board[row][i]==ch)return false;
-            
-            if(board[i][col]==ch)return false;
-            
-            if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)]==ch) return false;
-            
-            
-        }
-        return true;
-        
-        
-        
-    }
-    
-    
     public void solveSudoku(char[][] board) {
         solve(board);
     }
+    
+public boolean solve(char[][] matrix) {
+        
+for(int i=0;i<matrix.length;i++)
+{
+for(int j=0;j<matrix[i].length;j++)
+{
+
+if(matrix[i][j]=='.')//empty cell
+{
+for(char val='1';val<='9';val++)
+{
+if(isfeasible(i,j,matrix,val)){
+matrix[i][j]=val;
+if(solve(matrix))return true;//tail recursion
+else
+{
+    matrix[i][j]='.';
 }
+}
+
+}
+
+}
+    if(matrix[i][j]=='.')return false;
+}
+
+}
+return true;
+}
+public boolean isfeasible(int row,int col,char[][]matrix,char val)
+{
+
+for(int i=0;i<matrix.length;i++)
+{
+if(matrix[row][i]==val)return false;
+if(matrix[i][col]==val)return false;
+if(matrix[3*(row/3)+i/3][3*(col/3)+i%3]==val)return false;
+}
+    return true;
+}
+
+
+
+    }
