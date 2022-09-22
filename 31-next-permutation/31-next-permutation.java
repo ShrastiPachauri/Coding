ceb241da43de271b6af7e8b15ab6caf4631264af
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-09-22 15:18:23 +0530

    Time: 1 ms (92.17%), Space: 43.9 MB (30.69%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,70 +1,42 @@
 class Solution {
-    
-    public void reverse(int start,int[]nums)
-    {
-        
-        for(int i=start+1,j=nums.length-1;i<=j;i++,j--)//reversing the suffix
-        {
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-          
-        }
-        
-     
-        
-    }
-    
-   
-    public void nextPermutation(int[] nums) {
-        int start=-1;
-        if(nums.length<=1)
-        {
-            return;
-        }
-        
-         if(nums.length==2)//swap the last two values in case of completely incresing 
-        {
-            int temp=nums[nums.length-1];
-            nums[nums.length-1]=nums[nums.length-2];
-            nums[nums.length-2]=temp;
-            return;
-        }
-        
-        
-        for(int i=nums.length-2;i>=0;i--)
-        {
-            if(nums[i]<nums[i+1])
-            {
-                start=i;//pivot
-                
-        if(start>=0)
-        {
-            for(int j=nums.length-1;j>start;j--)
-        {
-            if(nums[j]>nums[start])//start -1 must exist
-            {
-                 int temp=nums[start];
-            nums[start]=nums[j];
-            nums[j]=temp;
-                
-                reverse(start,nums);
-                return;
-            }
-         
-        }
-        }
-            }
-        }
-        
-       
-      if(start==-1)//incresing array
-      {
-          reverse(-1,nums);
-      }
-      
-       
-        
-       
-    }
+public void reverse(int[] s,int in){
+int j=s.length-1;
+    int i=in;
+while(i<j){
+int x=s[i];
+s[i]=s[j];
+s[j]=x;
+i++;
+j--;
 }
+}
+    
+    
+    
+    
+public void nextPermutation(int[] s) {
+        
+
+if(s.length==1)return ;
+int index=-1;//mark index default value
+
+   int i=s.length-2; 
+    
+while(i>=0 && s[i]>=s[i+1])i--;//peechee se bhi increasing seq hota hai
+   
+
+ int j=s.length-1;
+    if(i>=0)
+    {while( s[j]<=s[i])j--;
+
+//swap
+    int x=s[i];
+    s[i]=s[j];
+    s[j]=x;
+    }
+    //reverse
+    reverse(s,i+1);
+ 
+}
+     
+    }
