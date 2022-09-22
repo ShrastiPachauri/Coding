af34fe6d6be6ccef6e39330559d776d6416fdaaa
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-09-22 16:54:47 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (68.29%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,59 +1,34 @@
 class Solution {
-    
-   
-    
     public boolean searchMatrix(int[][] matrix, int target) {
-        
-        
-        //decide row
-        int row=0;
-        for(int i=0;i<matrix.length;i++)
-        {
-            if(target<=matrix[i][matrix[0].length-1])
-            {
-                row=i;
-               
-                break;
-            }
-        }
-        //bsearch
-         for(int i=0;i<matrix[0].length;i++)
-         {
-             if(target<matrix[row][0])
-             {
-                 return false;
-             }
-             
-             int l=0;
-             int h=matrix[0].length-1;
-             int mid=l+(h-l)/2;
-            
-             while(l<=h)
-             {
-                 mid=l+(h-l)/2;
-                 if(matrix[row][mid]==target)
-                 {
-                      //System.out.println("target found "+i);
-                     return true;
-                 }
-                 if(matrix[row][mid]<target)
-                 {
-                     l=mid+1;
-                 }
-                 else
-                 {
-                     h=mid-1;
-                 }
-                 
-                 
-                 
-             }
-             
-             
-             
-             
-             
-         }
+    int row=-1;
+for(int i=0;i<matrix.length;i++){
+if(matrix[i][matrix[0].length-1]>= target){
+row=i;
+break;
+}
+}
+if(row==-1)return false;//row not found
+int low=0;
+int high=matrix[0].length-1;
+int mid=low +(high-low)/2;
+while(low<=high){
+
+ mid=low +(high-low)/2;
+if(matrix[row][mid]==target){
+return true;
+
+}
+else if(matrix[row][mid]>target)
+{
+high=mid-1;
+
+}
+else
+{
+low=mid+1;
+}
+}
         return false;
 }
+
 }
