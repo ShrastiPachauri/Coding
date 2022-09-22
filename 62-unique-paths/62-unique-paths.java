0da0a526fc6d37aad6d5a6ded92985ffd56fc748
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-09-22 19:41:30 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (98.15%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -2,48 +2,36 @@ class Solution {
     
     int[][]dp;
 
-    public int f(int m,int n,int i,int j)
-    {
-        if(i+j==(m+n-2))
-        {
-            return 1;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        
-       int x=0;
-        int y=0;
-        
-        if(i<m-1)
-        {x=f(m,n,i+1,j);}
-        
-       if(j<n-1)
-       {y=f(m,n,i,j+1);}
-        
-       
-    dp[i][j]=x+y;
-    
-        
-        return dp[i][j];
-        
-        
-        
-    }
-    
+public int possiblepath(int m,int n,int i,int j,int[][]dp){
+
+if(dp[i][j]!=-1)return dp[i][j];
+if(i==m-1 && j==n-1){
+return 1;
+}
+int x=0,y=0;
+if(i<m-1){
+ x=possiblepath(m,n,i+1,j,dp);}
+
+if(j<n-1){
+ y=possiblepath(m,n,i,j+1,dp);
+}
+
+dp[i][j]= x+y;
+return dp[i][j];
+
+
+}
+
+
     public int uniquePaths(int m, int n) {
-        
-        dp=new int[m+1][n+1];
-        
-        for(int i=0;i<=m;i++)
-        {
-            for(int j=0;j<=n;j++)
-            {
-                dp[i][j]=-1;
-            }
-        }
-        int res=f(m,n,0,0);
-        return res;
+      dp=new int[m+1][n+1];
+for(int i=0;i<=m;i++)
+{
+Arrays.fill(dp[i],-1);
+}
+return possiblepath(m,n,0,0,dp);
+
+
+
     }
 }
