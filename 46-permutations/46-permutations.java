3cb119e8df45f619954bf86b36ded49b04fa5a76
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-09-23 22:38:07 +0530

    Time: 6 ms (9.51%), Space: 44.8 MB (38.58%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,49 +1,40 @@
 class Solution {
     
-    HashSet<List<Integer>> ans;
-    
-    
-    public void f(int[]nums,int index)
-    {
-        
-        if(index==nums.length)
-        { List<Integer> l=new ArrayList<Integer>();
-        
-            for (int i = 0; i < nums.length; i++) {
-                l.add(nums[i]);
-            }
-            ans.add(l);
-            return;
-          
-        }
-        
-        for(int i=index;i<nums.length;i++)
-        {
-            int temp=nums[index];
-            nums[index]=nums[i];
-            nums[i]=temp;
-                
-                f(nums,index+1);
-            int t=nums[index];//back to original configuration
-            nums[index]=nums[i];
-            nums[i]=t;
-            
-        }
-       
+public void swap(int ind1,int ind2,int[]nums){
+int temp=nums[ind1];
+nums[ind1]=nums[ind2];
+nums[ind2]=temp;
+
+}
+ 
+public List<List<Integer>>permutations(int[]nums,int index,List<List<Integer>>ans){
+if(index>=nums.length)return ans;
+for(int i=index;i<nums.length;i++){
+swap(index,i,nums);
+permutations(nums,index+1,ans);
+swap(index,i,nums);
+}
+List<Integer>l=new ArrayList<Integer>();
+    for(int x:nums){
+        l.add(x);
     }
-    
+    ans.add(l);
+    return ans;
+}
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<List<Integer>>();
+permutations(nums,0,ans);
         
-        ans=new HashSet<List<Integer>>();
-        f(nums,0);
-        List<List<Integer>> answer=new ArrayList<List<Integer>>();
         
-        for(List<Integer> x:ans)
-        {
-            answer.add(x);
+        HashSet<List<Integer>>hs=new HashSet<List<Integer>>();
+        for(int i=0;i<ans.size();i++){
             
+            hs.add(ans.get(i));
         }
-        return answer;
+        ans=new ArrayList<List<Integer>>();
+        ans.addAll(hs);
+        return ans;
         
+
     }
 }
