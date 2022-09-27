3e5e961368ed167ebbfd532ea2f6cc1e46484dea
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-09-27 12:46:19 +0530

    Time: 20 ms (64.08%), Space: 46.3 MB (64.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,58 +1,36 @@
 class Solution {
+    //abcde
+    //ace
     
-   int [][]matrix;
-    
-    public int f(String t1,String t2,int ind1,int ind2)
-    {
+    int[][]dp;
+    public int lcs(String text1,String text2,int[][]dp,int i,int j){
         
-        if(ind1==t1.length() || ind2==t2.length())
-        {
-            return 0;
-        }
-        int len=0;
-        
-        if(matrix[ind1][ind2]!=-1)
-        {
-            return matrix[ind1][ind2];
-        }
-        
-        
-        
-        
-        //match
-        if(t1.charAt(ind1)==t2.charAt(ind2))
-        {
-            len=1+f(t1,t2,ind1+1,ind2+1);
+        if(i==text1.length()||j==text2.length())return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int p=0,np1=0,np2=0;
+        if(text1.charAt(i)==text2.charAt(j)){
+           p=1+lcs(text1,text2,dp,i+1,j+1);
         }
         else
         {
-        len=len+Math.max(f(t1,t2,ind1+1,ind2),f(t1,t2,ind1,ind2+1));
+            
+          np1= lcs(text1,text2,dp,i,j+1);
+            
+            np2= lcs(text1,text2,dp,i+1,j);
+            
+            
         }
-        
-        matrix[ind1][ind2]=len;
-        return  matrix[ind1][ind2];
+        dp[i][j]= (int)Math.max(p,(int)Math.max(np1,np2));
+        return dp[i][j];
     }
-    
-    
     
     public int longestCommonSubsequence(String text1, String text2) {
         
-        int n=Math.max(text2.length(),text1.length());
-        matrix=new int[n+1][n+1];
-        
-        for(int i=0;i<matrix.length;i++)
-        {
-            for(int j=0;j<matrix[0].length;j++)
-            {matrix[i][j]=-1;
-            }
+       int[][]dp=new int[text1.length()+1][text2.length()+1];
+        for(int[]x:dp){
+            Arrays.fill(x,-1);
         }
-        
-        return f(text1,text2,0,0);
-        
-        
-        
-        
-        
+         return lcs(text1,text2,dp,0,0);
         
     }
 }
