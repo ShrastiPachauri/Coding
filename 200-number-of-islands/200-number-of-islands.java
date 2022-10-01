fa13f7a2c30a1a657721fab5e892284a64c3dcbb
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-10-02 00:19:22 +0530

    Time: 11 ms (22.31%), Space: 58.4 MB (8.02%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,48 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int cnt=0;
-        
-        for(int i=0;i<grid.length;i++)
-        {
-            for(int j=0;j<grid[0].length;j++)
-            {
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 
-                if(grid[i][j]=='1')
-                {
-                    visit(grid,i,j);
+                if(grid[i][j]=='1'){
+                    trackLand(grid,i,j);
                     cnt++;
                 }
-                
             }
-            
         }
-        
-        
         return cnt;
-        
     }
-    
-    
-    public void visit(char[][]grid,int i,int k)
-    {
-        if(i<0||i>=grid.length||k>=grid[0].length||k<0||grid[i][k]=='0')
-        {
-            return;
-        }
         
-        if(grid[i][k]=='1')
-        { grid[i][k]='0';
-            visit(grid,i-1,k);
-            visit(grid,i+1,k);
-            visit(grid,i,k+1);
-            visit(grid,i,k-1);
-           
-        }
-        
-        
+        public void trackLand(char[][]grid,int r,int c){
+            
+            if(r<0||c<0||r>=grid.length||c>=grid[0].length)return;
+            
+            if(grid[r][c]=='1'){
+                grid[r][c]='0';
+            trackLand(grid,r+1,c);
+            trackLand(grid,r,c+1);
+            trackLand(grid,r-1,c);
+            trackLand(grid, r,c-1);
+            }
+            }
         
         
         
         
     }
-}
