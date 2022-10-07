9bedeed1c3be43749ce6618fa63ebf8fb9b98994
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-10-07 16:39:15 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (97.28%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,39 +1,25 @@
 class Solution {
     public int maxProfit(int[] prices) {
         
-        int netProfit=0;
-        int start=prices[0];
-        int end=prices[0];
-        for(int i=1;i<prices.length;i++)
-        {
+        int start=0;
+        int end=0;
+        int profit=0;
+        for(int i=0;i<prices.length;i++){
             
-           if(prices[i]<end)
-            {
-                 netProfit+=(end-start);
-                System.out.println("netProfit  "+netProfit);
-                    start=prices[i];
-                    end=prices[i];
+            if(prices[i]>prices[end]){
+                end=i;
+            }
+            if(prices[i]<prices[end]){
                 
-            }
-            else if(prices[i]<start)
-            {
-                start=prices[i];
-                end=prices[i];
-                //System.out.println("start and end are "+start+" "+end);
-            }
-            else if(prices[i]>end)
-            {
-                end=prices[i];
+                profit+=(prices[end]-prices[start]);//1 share profit
+                start=i;
+                end=i;
                
             }
-            
-            
-            
-            
-            
         }
-        netProfit+=(end-start);//understand welll  ;) last stock transaction must be included
-        return netProfit;
+        
+        profit+=(prices[end]-prices[start]);
+        return profit;
         
         
     }
