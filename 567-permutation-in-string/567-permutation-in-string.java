704021685682fc10bbd5c5b30c610d333f4c5c94
ShrastiPachauri <67330334+ShrastiPachauri@users.noreply.github.com>
2022-10-08 23:27:34 +0530

    Time: 1833 ms (8.09%), Space: 334.5 MB (5.01%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s2.length()<s1.length())return false;
+        
+        char[]st1=s1.toCharArray();
+        Arrays.sort(st1);
+        int last=s2.length()-s1.length();
+        for(int i=0;i<=last;i++){
+            
+           String curr=s2.substring(i,i+s1.length());
+            char[]arr=curr.toCharArray();
+            Arrays.sort(arr);
+            if(Arrays.equals(st1,arr)){
+                return true;
+            }
+            
+        }
+        return false;
+        
+    }
+}
